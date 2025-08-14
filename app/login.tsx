084da77eb90b08acf11fcc69da0b26fe57cb9422
Mayo <robertmayo.elumba@gmail.com>
2025-08-14 23:01:58 +0800

    Added Folders and Login Page

---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function Login() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-blue-800">Login Page</Text>
+    </View>
+  );
+}
